510acc37f48346663bfccdfc2605259e76424787
Moyses Azevedo <m.mirazev@gmail.com>
2024-11-14 08:34:23 -0300

    refactor: remove ViewController

--- Coordinator/Coordinator/Coordinator.swift
+++ Coordinator/Coordinator/Coordinator.swift
@@ -1,13 +1,6 @@
 import UIKit
 import Home
 
-public class ViewController: UIViewController {
-    public override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .red
-    }
-}
-
 final public class Coordinator: CoordinatorProtocol {
     var nav: UINavigationController
 
