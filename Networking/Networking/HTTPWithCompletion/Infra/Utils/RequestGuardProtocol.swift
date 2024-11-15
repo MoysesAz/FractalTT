f3406363ad0913171d5ce7f4f7320f1af06a3d35
Moyses Azevedo <m.mirazev@gmail.com>
2024-11-15 12:37:26 -0300

    feat(utils): add auxiliary protocol

---
+++ Networking/Networking/HTTPWithCompletion/Infra/Utils/RequestGuardProtocol.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+protocol RequestGuardProtocol {}
+
+extension RequestGuardProtocol {
+    public func validateURLString(_ url: String?) throws -> URL {
+        guard let urlString = url else {
+            Log.message("Url is Null", .failure)
+            throw NetworkErrors.nullURL
+        }
+        guard let newURL = URL(string: urlString) else {
+            Log.message("INVALID URL: \(urlString)", .failure)
+            throw NetworkErrors.invalidURL
+        }
+        Log.message("VALID URL: \(urlString)", .success)
+        return newURL
+    }
+
+    public func validateParameters(_ parameters: [String : Any]) throws -> Data {
+        do {
+            Log.message("Parameters encoded as JSON: \(parameters)", .success)
+            let data = try JSONSerialization.data(withJSONObject: parameters, options: [])
+            return data
+        } catch {
+            Log.message("Failed to encode parameters: \(error.localizedDescription)", .failure)
+            throw NetworkErrors.encodingError
+        }
+    }
+}
---
+++ Networking/Networking/HTTPWithCompletion/Infra/Utils/URLRequestWithCompletionProtocol.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+protocol URLRequestWithCompletionProtocol {}
+
+extension URLRequestWithCompletionProtocol {
+    public func request<T: Decodable>(_ urlRequest: URLRequest, completion: @escaping ((Result<T, Error>) -> Void)) {
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+
+            if let error = error {
+                Log.message(error.localizedDescription, .failure)
+                completion(.failure(NetworkErrors.networkFailure(message: error.localizedDescription)))
+                return
+            }
+
+            if let response = response as? HTTPURLResponse {
+                Log.response(response)
+            }
+
+            if let data = data {
+                do {
+                    let decodedResponse = try JSONDecoder().decode(T.self, from: data)
+                    completion(.success(decodedResponse))
+                } catch {
+                    Log.message("Decoding error: " + error.localizedDescription, .failure)
+                    completion(.failure(NetworkErrors.decodingError))
+                }
+            } else {
+                Log.message("No data received", .failure)
+                completion(.failure(NetworkErrors.noData))
+            }
+        }
+        task.resume()
+    }
+}
