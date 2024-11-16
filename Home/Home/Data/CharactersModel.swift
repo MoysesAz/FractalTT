c4ca86528e495e5f20acaae597900617965d1681
Moyses Azevedo <m.mirazev@gmail.com>
2024-11-16 20:01:47 -0300

    feat: add model

---
+++ Home/Home/Data/CharactersModel.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct CharacterModel: Codable {
+    var id: Int
+    var name: String
+    var status: String
+    var species: String
+    var type: String
+    var gender: String
+    var image: String
+}
---
+++ Home/Home/Data/RickaAdMortyApiEndPoint.swift
@@ -0,0 +1,8 @@
+//
+//  RickaAdMortyApiEndPoint.swift
+//  Home
+//
+//  Created by Moyses Miranda do Vale Azevedo on 16/11/24.
+//
+
+import Foundation
