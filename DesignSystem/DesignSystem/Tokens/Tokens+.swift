e096e81539504a955932fee50ca109ace17ec2bd
Moyses Azevedo <m.mirazev@gmail.com>
2024-11-16 09:58:50 -0300

    refactor: swiftlint

--- DesignSystem/DesignSystem/Tokens/Tokens+.swift
+++ DesignSystem/DesignSystem/Tokens/Tokens+.swift
@@ -3,11 +3,13 @@ import UIKit
 extension Luxux.Tokens.Colors {
     public struct Background {
         public static var lxPrimary: UIColor {
-            BugFixColor.getColor(for: .init(red: 1, green: 1, blue: 1, alpha: 1), dark: .init(red: 0, green: 0, blue: 0, alpha: 1))
+            BugFixColor.getColor(for: .init(red: 1, green: 1, blue: 1, alpha: 1),
+                                 dark: .init(red: 0, green: 0, blue: 0, alpha: 1))
             //                    UIColor(named: "New/Background/Primary") ?? .red
         }
         public static var lxSecondy: UIColor {
-            BugFixColor.getColor(for: .init(red: 0.4, green: 0.4, blue: 0.4, alpha: 1), dark: .init(red: 0.4, green: 0.4, blue: 0.4, alpha: 1))
+            BugFixColor.getColor(for: .init(red: 0.4, green: 0.4, blue: 0.4, alpha: 1),
+                                 dark: .init(red: 0.4, green: 0.4, blue: 0.4, alpha: 1))
             //                    UIColor(named: "Colors/Background/Secondary")!
         }
     }
@@ -16,11 +18,13 @@ extension Luxux.Tokens.Colors {
 extension Luxux.Tokens.Colors {
     public struct Font {
         public static var lxPrimary: UIColor {
-            BugFixColor.getColor(for: .init(red: 0.4, green: 0.4, blue: 0.4, alpha: 1), dark: .init(red: 1, green: 1, blue: 1, alpha: 1))
+            BugFixColor.getColor(for: .init(red: 0.4, green: 0.4, blue: 0.4, alpha: 1),
+                                 dark: .init(red: 1, green: 1, blue: 1, alpha: 1))
         }
     }
-
 }
+
+
 
 extension Luxux.Tokens.Font {
         static func montserratMedium(_ size: CGFloat = UIFont.labelFontSize) -> UIFont {
@@ -53,4 +57,3 @@ extension Luxux.Tokens.Font {
             return customFont
         }
 }
-
