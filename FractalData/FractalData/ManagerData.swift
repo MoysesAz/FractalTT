35acaa8fd644a066bbb4ba6d8ce6326312c577c1
Moyses Azevedo <m.mirazev@gmail.com>
2024-11-17 20:40:52 -0300

    feat: add managerData in ProductDetails

--- FractalData/FractalData/ManagerData.swift
+++ FractalData/FractalData/ManagerData.swift
@@ -3,6 +3,9 @@ import Commons
 
 public protocol ManagerDataProtocol {
     var viewContext: NSManagedObjectContext? { get set }
+    func getAllProducts() -> [Products]?
+    func createProduct(tag: String, productDescription: String, product: String, image: Data) -> Bool
+    func deleteProduct(tag: String, productDescription: String, product: String, image: Data) -> Bool
     init(_ viewContext: NSManagedObjectContext?)
 }
 
@@ -13,7 +16,7 @@ public final class ManagerData: ManagerDataProtocol {
 
     public var viewContext: NSManagedObjectContext?
 
-    func getAllProducts() -> [Products]? {
+    public func getAllProducts() -> [Products]? {
         do {
             let products = try viewContext?.fetch(Products.fetchRequest())
             return products
@@ -23,35 +26,67 @@ public final class ManagerData: ManagerDataProtocol {
         return nil
     }
 
-    func createProduct(product: Products) {
+    public func createProduct(tag: String, productDescription: String, product: String, image: Data) -> Bool {
         guard let context = viewContext else {
             Log.message("ManagerData(createProduct) - no viewContext", .warning)
-            return
+            return false
         }
 
         let newProduct = Products(context: context)
-        newProduct.tag = product.tag
-        newProduct.productDescription = product.productDescription
-        newProduct.image = product.image
-        newProduct.product = product.product
+        newProduct.tag = tag
+        newProduct.productDescription = productDescription
+        newProduct.image = image
+        newProduct.product = product
 
         do {
             try context.save()
             Log.message("ManagerData(createProduct) product save", .success)
+            return true
+
         } catch {
             Log.message("ManagerData(createProduct) - context save -> \(error)", .failure)
+            return false
         }
 
     }
 
-    func deleteProduct(product: Products) {
-        viewContext?.delete(product)
+    public func deleteProduct(tag: String,
+                              productDescription: String,
+                              product: String,
+                              image: Data) -> Bool {
+
+        guard let context = viewContext else {
+            Log.message("ManagerData(deleteProduct) - no viewContext", .warning)
+            return false
+        }
+
+        // Buscar o produto no contexto
+        let fetchRequest: NSFetchRequest<Products> = Products.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "tag == %@ AND productDescription == %@ AND product == %@ AND image == %@", tag, productDescription, product, image as CVarArg)
 
         do {
-            try viewContext?.save()
-            Log.message("ManagerData(deleteProduct) product save", .success)
+            let products = try context.fetch(fetchRequest)
+
+            // Verificar se o produto foi encontrado
+            if let productToDelete = products.first {
+                context.delete(productToDelete)
+
+                do {
+                    try viewContext?.save()
+                    Log.message("ManagerData(deleteProduct) product deleted", .success)
+                    return true
+                } catch {
+                    Log.message("ManagerData(deleteProduct) - context save -> \(error)", .failure)
+                    return false
+                }
+            } else {
+                Log.message("ManagerData(deleteProduct) - product not found", .warning)
+                return false
+            }
         } catch {
-            Log.message("ManagerData(deleteProduct) - context save -> \(error)", .failure)
+            Log.message("ManagerData(deleteProduct) - fetch request failed -> \(error)", .failure)
+            return false
         }
     }
+
 }
--- ProductDetails/ProductDetails/ProductDetailsViewModel.swift
+++ ProductDetails/ProductDetails/ProductDetailsViewModel.swift
@@ -1,15 +1,42 @@
 import Foundation
+import FractalData
 
 public protocol ProductDetailsModelProtocol {
+    init(managerData: ManagerDataProtocol?)
+    func saveProduct(tagTitle: String,
+                     productName: String,
+                     productDescription: String,
+                     image: Data)
+    func delete(tagTitle: String,
+                productName: String,
+                productDescription: String,
+                image: Data)
 }
 
 public class ProductDetailsModel {
-    public init () {
-        
-    }
+    var managerData: ManagerDataProtocol?
 
+    public required init(managerData: ManagerDataProtocol? = nil) {
+        self.managerData = managerData
+    }
 }
 
 extension ProductDetailsModel: ProductDetailsModelProtocol {
+    public func saveProduct(tagTitle: String, productName: String, productDescription: String, image: Data) {
+        _ = managerData?.createProduct(tag: tagTitle,
+                                   productDescription: productDescription,
+                                   product: productName,
+                                   image: image)
+    }
+
+    public func delete(tagTitle: String,
+                       productName: String,
+                       productDescription: String,
+                       image: Data) {
+        _ = managerData?.deleteProduct(tag: tagTitle,
+                                       productDescription: productDescription,
+                                       product: productName,
+                                       image: image)
+    }
 
 }
