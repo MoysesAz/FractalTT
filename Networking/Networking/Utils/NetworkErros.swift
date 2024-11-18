b24f7077328b3b28189ef5efa2990659741c5096
Moyses Azevedo <m.mirazev@gmail.com>
2024-11-18 08:39:05 -0300

    refactor: add string response

--- Networking/Networking/Utils/NetworkErros.swift
+++ Networking/Networking/Utils/NetworkErros.swift
@@ -1,11 +1,27 @@
 import Foundation
 
-public enum NetworkErrors: Error {
+enum NetworkErrors: Error {
     case nullURL
     case invalidURL
-    case networkFailure(message: String)
-    case unauthorized
-    case decodingError
     case encodingError
+    case decodingError
     case noData
+    case networkFailure(message: String)
+
+    var localizedDescription: String {
+        switch self {
+        case .nullURL:
+            return "URL is null"
+        case .invalidURL:
+            return "Invalid URL format"
+        case .encodingError:
+            return "Failed to encode parameters"
+        case .decodingError:
+            return "Failed to decode response"
+        case .noData:
+            return "No data received from server"
+        case .networkFailure(let message):
+            return "Network failure: \(message)"
+        }
+    }
 }
